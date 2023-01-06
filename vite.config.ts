1587b1f4088130ba2454cf2ab862d1f5f28ac639
Egor <vfjjfggg@gmail.com>
2023-01-06 19:35:16 +0400

    1.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -4,35 +4,64 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: "Calculator",
-        short_name: "Calculator",
-        description: "Calculator application",
-        orientation: "any",
-        theme_color: "#2196f3",
-        background_color: "#2196f3",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        icons: [
-          {
-            src: "/calculator-icon.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
-        ]
-      }
-    })
-  ],
-  build: {
-    target: "esnext"
-  }
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: "Calculator",
+                short_name: "Calculator",
+                description: "Calculator application",
+                orientation: "any",
+                theme_color: "#2196f3",
+                background_color: "#2196f3",
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "/calculator-icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable"
+                    }
+                ]
+            },
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                        },
+                        {
+                        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'gstatic-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            },
+                        }
+                    }
+                ]
+            }
+        })
+    ]
 })
